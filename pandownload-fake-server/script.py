1be4a9ef0ba5597a28026698beeef3053e9cc4d0
JK88-1337 <joekangaus@gmail.com>
2020-04-17 14:30:01 +1000

    fixed typo

--- pandownload-fake-server/script.py
+++ pandownload-fake-server/script.py
@@ -8,8 +8,8 @@ Protocol     = "HTTP/1.0"
 if sys.argv[1:]:
     port = int(sys.argv[1])
 else:
-    port = 80 #defalt port (you shouldn't be changing this one unless you know what you are doing)
-server_address = ('127.0.0.1', port) #defalt ip address
+    port = 80 #default port (you shouldn't be changing this one unless you know what you are doing)
+server_address = ('127.0.0.1', port) #default ip address
  
 HandlerClass.protocol_version = Protocol
 httpd = ServerClass(server_address, HandlerClass)
