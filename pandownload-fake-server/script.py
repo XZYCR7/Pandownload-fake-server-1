4822820b53324e30ded411fa6626e37073876a35
JK88-1337 <joekangaus@gmail.com>
2020-04-17 14:10:41 +1000

    upload

---
+++ pandownload-fake-server/script.py
@@ -0,0 +1,19 @@
+import sys
+import BaseHTTPServer
+from SimpleHTTPServer import SimpleHTTPRequestHandler
+HandlerClass = SimpleHTTPRequestHandler
+ServerClass  = BaseHTTPServer.HTTPServer
+Protocol     = "HTTP/1.0"
+ 
+if sys.argv[1:]:
+    port = int(sys.argv[1])
+else:
+    port = 80 #defalt port (you shouldn't be changing this one unless you know what you are doing)
+server_address = ('127.0.0.1', port) #defalt ip address
+ 
+HandlerClass.protocol_version = Protocol
+httpd = ServerClass(server_address, HandlerClass)
+ 
+sa = httpd.socket.getsockname()
+print "Serving HTTP on", sa[0], "port", sa[1], "..."
+httpd.serve_forever()
